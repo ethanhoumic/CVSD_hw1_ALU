5547eb76d2d39e5cd4a766767f9d6d04106cde5a
ethanhoumic <ethanhoumic@gmail.com>
2025-09-22 00:49:13 +0800

    LRCW done, delete redundant o_data_r

--- 01_RTL/alu.v
+++ 01_RTL/alu.v
@@ -17,9 +17,9 @@ module alu #(
     output        [DATA_W-1:0] o_data
 );
 
-    localparam S_IDLE = 2'b00;
-    localparam S_BUSY = 2'b01;
-    localparam S_DONE = 2'b10;
+    localparam S_IDLE      = 2'b00;
+    localparam S_BUSY_LRCW = 2'b01;
+    localparam S_DONE      = 2'b10;
 
     localparam C_1 = 16'shff55;
     localparam C_2 = 16'sh0009;
@@ -30,6 +30,7 @@ module alu #(
 
     reg output_valid_r;
     reg output_busy_r;
+    reg [4:0] cpop_r;
     reg [1:0] state_r;
     reg [35:0] data_acc_r;
     reg [DATA_W-1:0] o_data_r;
@@ -52,28 +53,37 @@ module alu #(
     wire signed [96:0] sin_x5_w = $signed(C_2) * $signed(i_data_a) * $signed(i_data_a) * $signed(i_data_a) * $signed(i_data_a) * $signed(i_data_a);
     wire signed [96:0] sin_w = sin_x1_w + sin_x3_w + sin_x5_w;
     wire signed [16:0] rounded_sin_w = (sin_w + (97'sd1 <<< 79)) >>> 80;
-
     
     assign o_out_valid = output_valid_r;
     assign o_busy = output_busy_r;
-    assign o_data = (i_inst == 4'b0100) ? o_data_r : signed_o_data_r;
+    assign o_data = signed_o_data_r;
 
 /*--------------------------------------------------------- comb loop ---------------------------------------------------*/
 
     always @(*) begin
-        
     end
 
 /*--------------------------------------------------------- sequ loop ---------------------------------------------------*/
 
     always @(posedge i_clk or negedge i_rst_n) begin
         if (!i_rst_n) begin
+            cpop_r <= 0;
             output_valid_r <= 0;
             output_busy_r <= 0;
             data_acc_r <= 0;
-            o_data_r <= 0;
             signed_o_data_r <= 0;
             state_r <= S_IDLE;
+        end
+        else if (state_r == S_BUSY_LRCW) begin
+            if (cpop_r == 0) begin
+                output_valid_r <= 1;
+                output_busy_r <= 0;
+                state_r <= S_IDLE;
+            end
+            else begin
+                signed_o_data_r <= {signed_o_data_r[14:0], !signed_o_data_r[15]};
+                cpop_r <= cpop_r - 1;
+            end
         end
         else if (i_in_valid) begin
             if (state_r == S_IDLE) begin
@@ -96,16 +106,22 @@ module alu #(
                         output_valid_r <= 1;
                     end
                     4'b0100: begin
-                        o_data_r[15] <= i_data_a[15];
+                        signed_o_data_r[15] <= i_data_a[15];
                         for (i = 14; i >= 0; i = i - 1) begin
-                            o_data_r[i] = i_data_a[i + 1] ^ i_data_a[i];
+                            signed_o_data_r[i] <= i_data_a[i+1] ^ i_data_a[i];
                         end
                         output_valid_r <= 1;
+                    end
+                    4'b0101: begin
+                        cpop_r <= cpop_count(i_data_a);
+                        signed_o_data_r <= i_data_b;
+                        state_r <= S_BUSY_LRCW;
+                        output_valid_r <= 0;
+                        output_busy_r <= 1;
                     end
                     default: begin
                         output_valid_r <= output_valid_r;
                         signed_o_data_r <= signed_o_data_r;
-                        o_data_r <= o_data_r;
                     end
                 endcase
             end
@@ -150,6 +166,18 @@ module alu #(
             end
             else begin
                 long_overflow_check = i_data[35:0];
+            end
+        end
+        
+    endfunction
+
+    function [5:0] cpop_count;
+        input [DATA_W-1:0] i_data;
+        integer i;
+        begin
+            cpop_count = 0;
+            for (i = 0; i < 16; i = i + 1) begin
+                cpop_count = cpop_count + i_data[i];
             end
         end
         
