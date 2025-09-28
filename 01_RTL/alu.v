4081221b6e2e07460ce17be076c31ead056420be
ethanhoumic <ethanhoumic@gmail.com>
2025-09-28 15:49:57 +0800

    FSM seperated

--- 01_RTL/alu.v
+++ 01_RTL/alu.v
@@ -34,6 +34,7 @@ module alu #(
     reg [3:0] matrix_counter;
     reg [4:0] cpop_r;
     reg [1:0] state_r;
+    reg [1:0] next_state_r;
     reg [35:0] data_acc_r;
     reg [1:0] matrix_mem_r [0:7][0:7];
     reg signed [DATA_W-1:0] signed_o_data_r;
@@ -65,130 +66,137 @@ module alu #(
 /*--------------------------------------------------------- comb loop ---------------------------------------------------*/
 
     always @(*) begin
+        next_state_r = state_r;
+        case (state_r)
+            S_IDLE: begin
+                if (i_in_valid) begin
+                    case (i_inst)
+                        4'b0101: next_state_r = S_BUSY_LRCW;
+                        4'b1001: next_state_r = S_BUSY_INPUT;
+                        default: next_state_r = S_IDLE;
+                    endcase
+                end
+            end
+            S_BUSY_LRCW: begin
+                if (cpop_r == 0) next_state_r = S_IDLE;
+            end
+            S_BUSY_INPUT: begin
+                if (matrix_counter == 7 && i_in_valid) next_state_r = S_BUSY_OUTPUT;
+                else next_state_r = S_BUSY_INPUT;
+            end
+            S_BUSY_OUTPUT: begin
+                if (matrix_counter == 8) next_state_r = S_IDLE;
+                else next_state_r = S_BUSY_OUTPUT;
+            end
+        endcase
     end
 
 /*--------------------------------------------------------- sequ loop ---------------------------------------------------*/
 
     always @(posedge i_clk or negedge i_rst_n) begin
-        if (!i_rst_n) begin
-            matrix_counter <= 0;
-            cpop_r <= 0;
-            output_valid_r <= 0;
-            output_busy_r <= 0;
-            data_acc_r <= 0;
-            signed_o_data_r <= 0;
+        if (!i_rst_n)
             state_r <= S_IDLE;
+        else
+            state_r <= next_state_r;
+    end
+
+    always @(posedge i_clk or negedge i_rst_n) begin
+        if (!i_rst_n) begin
+            output_valid_r   <= 0;
+            output_busy_r    <= 0;
+            signed_o_data_r  <= 0;
+            cpop_r           <= 0;
+            matrix_counter   <= 0;
+            data_acc_r       <= 0;
             for (i = 0; i < 8; i = i + 1) begin
                 for (j = 0; j < 8; j = j + 1) begin
                     matrix_mem_r[i][j] <= 0;
                 end
             end
         end
-        else if (state_r == S_BUSY_LRCW) begin
-            if (cpop_r == 0) begin
-                output_valid_r <= 1;
-                output_busy_r <= 0;
-                state_r <= S_IDLE;
-            end
-            else begin
-                signed_o_data_r <= {signed_o_data_r[14:0], !signed_o_data_r[15]};
-                cpop_r <= cpop_r - 1;
-            end
-        end
-        else if (state_r == S_BUSY_INPUT && i_in_valid) begin
-            for (i = 0; i < 8; i = i + 1) begin
-                matrix_mem_r[i][matrix_counter] <= i_data_a[(i<<1)+:2];
-            end
-            if (matrix_counter == 7) begin
-                matrix_counter <= 0;
-                output_busy_r <= 1;
-                state_r <= S_BUSY_OUTPUT;
-            end
-            else begin
-                matrix_counter <= matrix_counter + 1;
-                state_r <= state_r;
-            end
-        end
-        else if (state_r == S_BUSY_OUTPUT) begin
-            if (matrix_counter == 8) begin
-                matrix_counter <= 0;
-                state_r <= S_IDLE;
-                output_valid_r <= 0;
-                output_busy_r <= 0;
-            end
-            else begin
-                signed_o_data_r <= {matrix_mem_r[7-matrix_counter][0], matrix_mem_r[7-matrix_counter][1], matrix_mem_r[7-matrix_counter][2], matrix_mem_r[7-matrix_counter][3], 
-                matrix_mem_r[7-matrix_counter][4], matrix_mem_r[7-matrix_counter][5], matrix_mem_r[7-matrix_counter][6], matrix_mem_r[7-matrix_counter][7]};
-                matrix_counter <= matrix_counter + 1;
-                output_valid_r <= 1;
-            end
-        end
-        else if (i_in_valid) begin
-            if (state_r == S_IDLE) begin
-                case (i_inst)
-                    4'b0000: begin
-                        signed_o_data_r <= overflow_check(sum_w);
+        else begin
+            case (state_r)
+                S_IDLE: begin
+                    output_valid_r <= 0;
+                    if (i_in_valid) begin
+                        case (i_inst)
+                            4'b0000: signed_o_data_r <= overflow_check(sum_w);
+                            4'b0001: signed_o_data_r <= overflow_check(diff_w);
+                            4'b0010: begin
+                                data_acc_r       <= long_overflow_check(mac_w);
+                                signed_o_data_r  <= overflow_check(rounded_mac_w);
+                            end
+                            4'b0011: signed_o_data_r <= overflow_check(rounded_sin_w);
+                            4'b0100: begin
+                                signed_o_data_r[15] <= i_data_a[15];
+                                for (i = 14; i >= 0; i = i - 1)
+                                    signed_o_data_r[i] <= i_data_a[i+1] ^ i_data_a[i];
+                            end
+                            4'b0101: begin
+                                cpop_r          <= cpop_count(i_data_a);
+                                signed_o_data_r <= i_data_b;
+                                output_busy_r   <= 1;
+                            end
+                            4'b0110: signed_o_data_r <= (i_data_b >= 16) ? i_data_a : i_data_a_con[i_data_b[3:0] +: 16];
+                            4'b0111: signed_o_data_r <= count_leading_zero(i_data_a);
+                            4'b1000: signed_o_data_r <= reverse_match4(i_data_a, i_data_b);
+                            4'b1001: begin
+                                for (i = 0; i < 8; i = i + 1) begin
+                                    matrix_mem_r[i][0] <= i_data_a[(i<<1)+:2];
+                                end
+                                matrix_counter <= 1;
+                            end
+                        endcase
+                        output_valid_r <= (i_inst != 4'b0101 && i_inst != 4'b1001);
+                    end
+                end
+
+                S_BUSY_LRCW: begin
+                    if (cpop_r != 0) begin
+                        signed_o_data_r <= {signed_o_data_r[14:0], !signed_o_data_r[15]};
+                        cpop_r          <= cpop_r - 1;
+                    end else begin
                         output_valid_r <= 1;
+                        output_busy_r  <= 0;
                     end
-                    4'b0001: begin
-                        signed_o_data_r <= overflow_check(diff_w);
-                        output_valid_r <= 1;
-                    end
-                    4'b0010: begin
-                        data_acc_r <= long_overflow_check(mac_w);
-                        signed_o_data_r <= overflow_check(rounded_mac_w);
-                        output_valid_r <= 1;
-                    end
-                    4'b0011: begin
-                        signed_o_data_r <= overflow_check(rounded_sin_w);
-                        output_valid_r <= 1;
-                    end
-                    4'b0100: begin
-                        signed_o_data_r[15] <= i_data_a[15];
-                        for (i = 14; i >= 0; i = i - 1) begin
-                            signed_o_data_r[i] <= i_data_a[i+1] ^ i_data_a[i];
-                        end
-                        output_valid_r <= 1;
-                    end
-                    4'b0101: begin
-                        cpop_r <= cpop_count(i_data_a);
-                        signed_o_data_r <= i_data_b;
-                        state_r <= S_BUSY_LRCW;
-                        output_valid_r <= 0;
-                        output_busy_r <= 1;
-                    end
-                    4'b0110: begin
-                        signed_o_data_r <= (i_data_b >= 16) ? i_data_a : i_data_a_con[i_data_b[3:0] +: 16];
-                        output_valid_r <= 1;
-                    end
-                    4'b0111: begin
-                        signed_o_data_r <= (count_leading_zero(i_data_a));
-                        output_valid_r <= 1;
-                    end
-                    4'b1000: begin
-                        signed_o_data_r <= reverse_match4(i_data_a, i_data_b);
-                        output_valid_r <= 1;
-                    end
-                    4'b1001: begin
-                        state_r <= S_BUSY_INPUT;
+                end
+
+                S_BUSY_INPUT: begin
+                    if (i_in_valid) begin
                         for (i = 0; i < 8; i = i + 1) begin
-                            matrix_mem_r[i][0] <= i_data_a[(i<<1)+:2];
+                            matrix_mem_r[i][matrix_counter] <= i_data_a[(i<<1)+:2];
                         end
-                        matrix_counter <= matrix_counter + 1;
+                        if (matrix_counter == 7) begin
+                            matrix_counter <= 0;
+                            output_busy_r <= 1;
+                        end
+                        else begin
+                            matrix_counter <= matrix_counter + 1;
+                        end
+                    end
+                end
+
+                S_BUSY_OUTPUT: begin
+                    if (matrix_counter < 8) begin
+                        signed_o_data_r <= {
+                            matrix_mem_r[7-matrix_counter][0], matrix_mem_r[7-matrix_counter][1],
+                            matrix_mem_r[7-matrix_counter][2], matrix_mem_r[7-matrix_counter][3],
+                            matrix_mem_r[7-matrix_counter][4], matrix_mem_r[7-matrix_counter][5],
+                            matrix_mem_r[7-matrix_counter][6], matrix_mem_r[7-matrix_counter][7]
+                        };
+                        matrix_counter  <= matrix_counter + 1;
+                        output_valid_r  <= 1;
+                        output_busy_r <= 1;
+                    end else begin
+                        matrix_counter <= 0;
                         output_valid_r <= 0;
+                        output_busy_r  <= 0;
                     end
-                    default: begin
-                        output_valid_r <= output_valid_r;
-                        signed_o_data_r <= signed_o_data_r;
-                    end
-                endcase
-            end
-        end
-        else if (!i_in_valid) begin
-            output_valid_r <= 0;
-            signed_o_data_r <= signed_o_data_r;
+                end
+            endcase
         end
     end
+
 
 /*--------------------------------------------------------- function ----------------------------------------------------*/
 
